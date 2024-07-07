d38b7b80bb8feca2c055ff3998126d6a2a1f249f
dawoon <52641434+wwhynott@users.noreply.github.com>
2024-07-07 17:42:01 +0900

    [Silver I] Title: 출근, Time: 572 ms, Memory: 123364 KB -BaekjoonHub

---
+++ 백준/Silver/13903. 출근/출근.java
@@ -0,0 +1,95 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int R, C, N; // 행,열,델타배열 크기
+	static int[][] blockArr; // 보도블록 상태 배열
+	static int[] dr, dc; // 델타배열
+	static boolean[][] visit; // 방문 여부 배열
+	static int res;
+	static Queue<int[]> queue;
+	static boolean flag = false;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		blockArr = new int[R][C];
+		visit = new boolean[R][C];
+		res = Integer.MAX_VALUE;
+		queue = new LinkedList<>();
+
+		for (int r = 0; r < R; r++) {
+			StringTokenizer st2 = new StringTokenizer(br.readLine());
+			for (int c = 0; c < C; c++) {
+				blockArr[r][c] = Integer.parseInt(st2.nextToken());
+			}
+		}
+
+		N = Integer.parseInt(br.readLine());
+		dr = new int[N];
+		dc = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st3 = new StringTokenizer(br.readLine());
+
+			int nr = Integer.parseInt(st3.nextToken());
+			int nc = Integer.parseInt(st3.nextToken());
+
+			dr[i] = nr;
+			dc[i] = nc;
+		}
+
+		bfs();
+
+		// 해답이 없을 경우 어떻게 처리??????
+		if (flag) {
+			System.out.println(res);
+		} else {
+			System.out.println(-1);
+		}
+
+	}
+
+	static void bfs() {
+		for (int c = 0; c < C; c++) {
+			if (blockArr[0][c] == 1) {
+				int[] arr = { 0, c, 0 }; // r, c, 몇걸음 째 인지
+				queue.add(arr);
+				visit[0][c] = true;
+			}
+		}
+
+		while (!queue.isEmpty()) {
+			int[] next = queue.poll();
+			if (next[0] == R - 1) { // 회사 도착
+				// 루트부터 얼마나 왔는지 어떻게 확인?? -> 몇걸음째 왔는지도 저장 ㄱ?
+				res = Math.min(res, next[2]);
+				flag = true; // 해답 없는 경우 확인하기 위한 flag
+				return;
+			}
+
+			int cnt = next[2] + 1; // 한걸음 추가
+			for (int d = 0; d < N; d++) {
+				int nr = next[0] + dr[d];
+				int nc = next[1] + dc[d];
+				
+				if (nr >= 0 && nr < R && nc >= 0 && nc < C && !visit[nr][nc] && blockArr[nr][nc] == 1) {
+					int[] temp = { nr, nc, cnt };
+					queue.add(temp);
+					visit[nr][nc] = true;
+				}
+			}
+		}
+
+	}
+
+}
