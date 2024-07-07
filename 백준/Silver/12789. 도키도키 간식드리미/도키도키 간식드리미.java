a608b01f411299b566a04d571258ad0bfd862f4a
dawoon <52641434+wwhynott@users.noreply.github.com>
2024-07-07 17:48:58 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 136 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 학생 수
+		int N = Integer.parseInt(br.readLine());
+
+		// 현재 줄서있는곳 : queue
+		// 한 명씩 설 수 있는 공간 : stack
+
+		// 현재 줄서있는곳
+		Queue<Integer> curLine = new LinkedList<>();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			curLine.offer(Integer.parseInt(st.nextToken()));
+		}
+
+		// 한 명씩 서는 공간
+		Stack<Integer> stack = new Stack<>();
+
+		// 줄 순서
+		int turnCnt = 1; // 1번 부터~
+
+		while (!curLine.isEmpty()) {
+			// 줄 설 순서가 아닌 경우 스택으로 push, 줄 설 순서인 경우 날리기 & 줄 순서++
+			if (curLine.peek() == turnCnt) {
+				curLine.poll();
+				turnCnt++;
+			} else {
+				if (!stack.isEmpty()) {
+					if (stack.peek() == turnCnt) {
+						stack.pop();
+						turnCnt++;
+					} else {
+						stack.push(curLine.poll());
+					}
+				} else {
+					stack.push(curLine.poll());
+				}
+			}
+		}
+
+		// 스택에서 꺼내지는 순서가 줄 순서와 일치하지 않으면 Sad
+		while (!stack.isEmpty()) {
+			if (stack.peek() == turnCnt) {
+				stack.pop();
+				turnCnt++;
+			} else {
+				System.out.println("Sad");
+				break;
+			}
+		}
+
+		// 줄 순서 끊기지 않고 while문 탈출
+		if (turnCnt == N + 1) {
+			System.out.println("Nice");
+		}
+	}
+}
